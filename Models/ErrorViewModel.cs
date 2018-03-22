1d4a24d4ca7f0ed07d4e65fe1a85fd1166488326
Luis Jimenez <Guero@Luiss-MacBook-Pro-2.local>
2018-03-21 21:43:17 -0500

    commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MIDTERM2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
